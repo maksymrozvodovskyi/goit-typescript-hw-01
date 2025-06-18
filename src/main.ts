d044c72df0085f7f5718f61ef52018503f818fee
maksymrozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-06-18 15:57:26 +0300

    add generic

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
 
+// Basic
 import "./basic/1";
 import "./basic/2";
 import "./basic/3";
@@ -7,3 +8,11 @@ import "./basic/4";
 import "./basic/5";
 import "./basic/6";
 import "./basic/7";
+
+//Generic
+import "./generic/1";
+import "./generic/2";
+import "./generic/3";
+import "./generic/4";
+import "./generic/5";
+import "./generic/6";
